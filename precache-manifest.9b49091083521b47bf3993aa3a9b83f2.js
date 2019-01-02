3f730db7f12dc7a6de1a421713f7c8afc438b073
Pieter Wolmarans <pieter@pixelume.com>
2019-01-02 16:18:25 +0200

    Updates

---
+++ precache-manifest.9b49091083521b47bf3993aa3a9b83f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "45a290decd2f204244c680860a22a857",
+    "url": "/static/media/portfolio.45a290de.jpeg"
+  },
+  {
+    "revision": "c1aded761001d52bbc35f34a48e4b577",
+    "url": "/static/media/my-head-shot.c1aded76.jpg"
+  },
+  {
+    "revision": "f9406413fd8a0ecbc303c7e2d5cfe3b3",
+    "url": "/static/media/macbook_pro.f9406413.jpg"
+  },
+  {
+    "revision": "42f05e465f52b671a20611cfccdeb55a",
+    "url": "/static/media/eye-chart.42f05e46.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df4cc25d361d25ac9d96",
+    "url": "/static/js/main.df4cc25d.chunk.js"
+  },
+  {
+    "revision": "be1da2701b6a7037e21e",
+    "url": "/static/js/1.be1da270.chunk.js"
+  },
+  {
+    "revision": "df4cc25d361d25ac9d96",
+    "url": "/static/css/main.8d0406c6.chunk.css"
+  },
+  {
+    "revision": "897b2179e01290c1dc4ff3e4e008622f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.23dae75829a363c739c14234b92473d6.js"
+  "/precache-manifest.9b49091083521b47bf3993aa3a9b83f2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
